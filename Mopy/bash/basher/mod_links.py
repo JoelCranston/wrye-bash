JoelCranston/wrye-bash

commit 43ab0c1d6ac2fa3f81fac2cb9e7dfeeab0c6606f

Moving name text and tip to gui WIP EEE
EEE

re-add empty unloadedText if allowUnloaded == False (or rather drop,
only CBash_ListsMerger has it False !)

@@ -149,4 +147,3 @@ class PatchMerger(APatchMerger, ListPatcher): pass

-class CBash_PatchMerger(APatchMerger, CBash_ListPatcher):
-    unloadedText = "" # Cbash only
+class CBash_PatchMerger(APatchMerger, CBash_ListPatcher): pass

@@ -470,3 +470,2 @@ def buildPatch(self,log,progress):
 class CBash_CoblExhaustion(_ACoblExhaustion, _DefaultDictLog):
-    unloadedText = ""

@@ -666,3 +665,2 @@ def buildPatch(self,log,progress):
 class CBash_MFactMarker(_AMFactMarker, _DefaultDictLog):
-    unloadedText = u""

What is this tmp extension ??

+class CBash_RacePatcher(SpecialPatcher, CBash_ListPatcher):
     autoRe = re.compile(u'^UNDEFINED$', re.I | re.U)
@@ -1768,7 +1750,5 @@ def buildPatchLog(self,log):
             for srcMod in sorted(mod_npcsFixed):
-                if srcMod.cext == u'.tmp':
-                    name = srcMod.sbody
-                else:
-                    name = srcMod.s
-                log(u'* %s: %d' % (name,len(mod_npcsFixed[srcMod])))
+                log(u'* %s: %d' % (
+                    (srcMod.sbody if srcMod.cext == u'.tmp' else srcMod.s),
+                    len(mod_npcsFixed[srcMod])))
         for tweak in self.enabledTweaks:

More patcher_name

_patcher_text and property for unloadedText WIP

Needs docs for allowUnloaded... plus what does

+_(u'Any non-active, non-merged mods in the'
+                             u' following list will be IGNORED.')

exactly mean?

_patcher_name _Abstract_Patcher attr - needed for logs SSS

Drop self.scan/EditOrder - I don't see why define those per instance

@@ -55,4 +55,2 @@ def __init__(self, patcher_name, patchFile):
         """Initialization of common values to defaults."""
-        self.scanOrder = self.__class__.scanOrder
-        self.editOrder = self.__class__.editOrder
         self.isActive = True

Mopy/bash/basher/mod_links.py
@@ -1130,10 +1130,10 @@ class Mod_ListPatchConfig(_Mod_BP_Link):
                                              'bash.patch.configs', {})
         # Detect CBash/Python mode patch
         doCBash = configIsCBash(config)
-        patchers = [copy.deepcopy(x) for x in
+        _gui_patchers = [copy.deepcopy(x) for x in
                     (CBash_gui_patchers if doCBash else PBash_gui_patchers)]
-        patchers.sort(key=lambda a: a.__class__.name)
-        patchers.sort(key=lambda a: groupOrder[a.__class__.group])
+        _gui_patchers.sort(key=lambda a: a.__class__.patcher_name)
+        _gui_patchers.sort(key=lambda a: groupOrder[a.patcher_instance.group])
         #--Log & Clipboard text
         log = bolt.LogFile(StringIO.StringIO())
         log.setHeader(u'= %s %s' % (self._selected_item, _(u'Config')))
@@ -1155,7 +1155,7 @@ class Mod_ListPatchConfig(_Mod_BP_Link):
         else:
             log(u'Python')
             clip.write(u' ** Python\n')
-        for patcher in patchers:
+        for patcher in _gui_patchers:
             patcher.log_config(config, clip, log)
         #-- Show log
         clip.write(u'[/xml][/spoiler]')

Mopy/bash/basher/patcher_dialog.py
@@ -83,13 +83,13 @@ class PatchDialog(DialogWindow):
         self.patchInfo = patchInfo
         self._gui_patchers = [copy.deepcopy(p) for p in (
             CBash_gui_patchers if doCBash else PBash_gui_patchers)]
-        self._gui_patchers.sort(key=lambda a: a.__class__.name)
-        self._gui_patchers.sort(key=lambda a: groupOrder[a.__class__.group])
+        self._gui_patchers.sort(key=lambda a: a.__class__.patcher_name)
+        self._gui_patchers.sort(key=lambda a: groupOrder[a.patcher_type.group])
         for patcher in self._gui_patchers:
             patcher.getConfig(patchConfigs) #--Will set patcher.isEnabled
             patcher.SetIsFirstLoad(isFirstLoad)
         self.currentPatcher = None
-        patcherNames = [patcher.getName() for patcher in self._gui_patchers]
+        patcherNames = [patcher.patcher_name for patcher in self._gui_patchers]
         #--GUI elements
         self.gExecute = OkButton(self, label=_(u'Build Patch'))
         self.gExecute.on_clicked.subscribe(self.PatchExecute)
@@ -485,10 +485,8 @@ class PatchDialog(DialogWindow):
 
     def _set_tip_text(self, mouseItem):
         if 0 <= mouseItem < len(self._gui_patchers):
-            patcherClass = self._gui_patchers[mouseItem].__class__
-            tip = patcherClass.tip or re.sub(u'' r'\..*', u'.',
-                            patcherClass.text.split(u'\n')[0], flags=re.U)
-            self.gTipText.label_text = tip
+            gui_patcher = self._gui_patchers[mouseItem]
+            self.gTipText.label_text = gui_patcher.patcher_tip
         else:
             self.gTipText.label_text = self.defaultTipText
 

Mopy/bash/game/oblivion/patcher/__init__.py
@@ -25,28 +25,45 @@
 """This package contains the Oblivion specific patchers. This module
 contains the data structures that are dynamically set on a per game basis in
 bush."""
+# Note patcher_text and not _patcher_txt - later is a class var of the gui patchers
+
 from ....patcher import PatcherInfo as pi
 from .importers import RoadImporter, CBash_RoadImporter
 from .special import AlchemicalCatalogs, CBash_AlchemicalCatalogs, \
     SEWorldEnforcer, CBash_SEWorldEnforcer, CoblExhaustion, \
     CBash_CoblExhaustion, MFactMarker, CBash_MFactMarker
 
+_spedial_patchers = (
+    ("AlchemicalCatalogs", AlchemicalCatalogs, 'CBash_AlchemicalCatalogs'),
+    ("CBash_AlchemicalCatalogs", CBash_AlchemicalCatalogs,
+     'AlchemicalCatalogs'),
+    ("SEWorldEnforcer", SEWorldEnforcer, 'CBash_SEWorldEnforcer'),
+    ("CBash_SEWorldEnforcer", CBash_SEWorldEnforcer, 'SEWorldEnforcer')
+)
 gameSpecificPatchers = {
-    # special
-    "AlchemicalCatalogs": pi(AlchemicalCatalogs, 'CBash_AlchemicalCatalogs', AlchemicalCatalogs.patcher_text),
-    "CBash_AlchemicalCatalogs": pi(CBash_AlchemicalCatalogs, 'AlchemicalCatalogs', CBash_AlchemicalCatalogs.patcher_text),
-    "SEWorldEnforcer": pi(SEWorldEnforcer, 'CBash_SEWorldEnforcer', SEWorldEnforcer.patcher_text),
-    "CBash_SEWorldEnforcer": pi(CBash_SEWorldEnforcer, 'SEWorldEnforcer', CBash_SEWorldEnforcer.patcher_text),
-}
-gameSpecificListPatchers = {
-    # special
-    "CoblExhaustion": pi(CoblExhaustion, 'CBash_CoblExhaustion', CoblExhaustion.patcher_text),
-    "CBash_CoblExhaustion": pi(CBash_CoblExhaustion, 'CoblExhaustion', CBash_CoblExhaustion.patcher_text),
-    "MFactMarker": pi(MFactMarker, 'CBash_MFactMarker', MFactMarker.patcher_text),
-    "CBash_MFactMarker": pi(CBash_MFactMarker, 'MFactMarker', CBash_MFactMarker.patcher_text),
-}
+    pname: pi(ptype, twin, {'_patcher_txt': ptype.patcher_text,
+                            'patcher_name': ptype.patcher_name}) for
+    pname, ptype, twin in _spedial_patchers}
+
+_list_pacthers =(
+    ("CoblExhaustion", CoblExhaustion, 'CBash_CoblExhaustion'),
+    ("CBash_CoblExhaustion", CBash_CoblExhaustion, 'CoblExhaustion'),
+    ("MFactMarker", MFactMarker, 'CBash_MFactMarker'),
+    ("CBash_MFactMarker", CBash_MFactMarker, 'MFactMarker')
+)
+gameSpecificListPatchers =  {
+    pname: pi(ptype, twin, {'_patcher_txt': ptype.patcher_text,
+                            'patcher_name': ptype.patcher_name,
+                            'autoKey': ptype.autoKey}) for
+    pname, ptype, twin in _list_pacthers}
+
+_import_patchers = (
+    ('RoadImporter', RoadImporter, 'CBash_RoadImporter'),
+    ('CBash_RoadImporter', CBash_RoadImporter, 'RoadImporter')
+)
 game_specific_import_patchers = {
-    # importers
-    'RoadImporter': pi(RoadImporter, 'CBash_RoadImporter', RoadImporter.patcher_text),
-    'CBash_RoadImporter': pi(CBash_RoadImporter, 'RoadImporter', CBash_RoadImporter.patcher_text),
+    pname: pi(ptype, twin,
+              {'patcher_type': ptype, '_patcher_txt': ptype.patcher_text,
+               'patcher_name': ptype.patcher_name, 'autoKey': ptype.autoKey})
+    for pname, ptype, twin in _import_patchers
 }

Mopy/bash/game/oblivion/patcher/importers.py
@@ -30,9 +30,8 @@ __all__ = ['RoadImporter', 'CBash_RoadImporter']
 
 class _ARoadImporter(AImportPatcher):
     """Imports roads."""
-    name = _(u'Import Roads')
+    patcher_name = _(u'Import Roads')
     patcher_text = _(u"Import roads from source mods.")
-    tip = patcher_text
     autoKey = {u'Roads'}
 
 class RoadImporter(ImportPatcher, _ARoadImporter):
@@ -152,7 +151,7 @@ class CBash_RoadImporter(CBash_ImportPatcher, _ARoadImporter):
                 copyRoad = curRoad #Copy the current road over (its formID is acceptable)
             else:
                 #Ignore the record.
-                self.patchFile.patcher_mod_skipcount[self.name][
+                self.patchFile.patcher_mod_skipcount[self.patcher_name][
                     modFile.GName] += 1
                 return
 

Mopy/bash/game/oblivion/patcher/special.py
@@ -166,7 +166,7 @@ class AlchemicalCatalogs(_AAlchemicalCatalogs,Patcher):
                         buffWrite(u'\r\n')
                 book.text = re.sub(u'\r\n',u'<br>\r\n',buff.getvalue())
         #--Log
-        log.setHeader(u'= '+self.__class__.name)
+        log.setHeader(u'= ' + self._patcher_name)
         log(u'* '+_(u'Ingredients Cataloged') + u': %d' % len(id_ingred))
         log(u'* '+_(u'Effects Cataloged') + u': %d' % len(effect_ingred))
 
@@ -337,7 +337,7 @@ class CBash_AlchemicalCatalogs(_AAlchemicalCatalogs,CBash_Patcher):
         #--Log
         id_ingred = self.id_ingred
         effect_ingred = self.effect_ingred
-        log.setHeader(u'= '+self.__class__.name)
+        log.setHeader(u'= ' + self._patcher_name)
         log(u'* '+_(u'Ingredients Cataloged') + u': %d' % len(id_ingred))
         log(u'* '+_(u'Effects Cataloged') + u': %d' % len(effect_ingred))
 
@@ -358,7 +358,7 @@ class _ACoblExhaustion(SpecialPatcher):
     # TODO: readFromText differ only in (PBash -> CBash):
     # -         longid = (aliases.get(mod,mod),int(objectIndex[2:],16))
     # +         longid = FormID(aliases.get(mod,mod),int(objectIndex[2:],16))
-    name = _(u'Cobl Exhaustion')
+    patcher_name = _(u'Cobl Exhaustion')
     patcher_text = u'\n\n'.join(
         [_(u"Modify greater powers to use Cobl's Power Exhaustion feature."),
          _(u'Will only run if Cobl Main v1.66 (or higher) is active.')])
@@ -366,7 +366,7 @@ class _ACoblExhaustion(SpecialPatcher):
     autoKey = {u'Exhaust'}
 
     def _pLog(self, log, count):
-        log.setHeader(u'= ' + self.__class__.name)
+        log.setHeader(u'= ' + self._patcher_name)
         log(u'* ' + _(u'Powers Tweaked') + u': %d' % sum(count.values()))
         for srcMod in load_order.get_ordered(count.keys()):
             log(u'  * %s: %d' % (srcMod.s, count[srcMod]))
@@ -456,7 +456,6 @@ class CoblExhaustion(_ACoblExhaustion,ListPatcher):
         self._pLog(log, count)
 
 class CBash_CoblExhaustion(_ACoblExhaustion, _DefaultDictLog):
-    unloadedText = ""
 
     #--Config Phase -----------------------------------------------------------
     def initPatchFile(self, patchFile):
@@ -530,7 +529,7 @@ class CBash_CoblExhaustion(_ACoblExhaustion, _DefaultDictLog):
 #------------------------------------------------------------------------------
 class _AMFactMarker(SpecialPatcher):
     """Mark factions that player can acquire while morphing."""
-    name = _(u'Morph Factions')
+    patcher_name = _(u'Morph Factions')
     patcher_text = u'\n\n'.join(
         [_(u"Mark factions that player can acquire while morphing."),
          _(u"Requires Cobl 1.28 and Wrye Morph or similar.")])
@@ -540,7 +539,7 @@ class _AMFactMarker(SpecialPatcher):
     autoKey = {u'MFact'}
 
     def _pLog(self, log, changed):
-        log.setHeader(u'= ' + self.__class__.name)
+        log.setHeader(u'= ' + self._patcher_name)
         self._srcMods(log)
         log(u'\n=== ' + _(u'Morphable Factions'))
         for mod in load_order.get_ordered(changed):
@@ -645,7 +644,6 @@ class MFactMarker(_AMFactMarker,ListPatcher):
         self._pLog(log, changed)
 
 class CBash_MFactMarker(_AMFactMarker, _DefaultDictLog):
-    unloadedText = u""
 
     #--Config Phase -----------------------------------------------------------
     def initPatchFile(self, patchFile):
@@ -810,7 +808,7 @@ class SEWorldEnforcer(_ASEWorldEnforcer,Patcher):
                 record.conditions.insert(0,condition)
                 keep(record.fid)
                 patched.append(record.eid)
-        log.setHeader('= '+self.__class__.name)
+        log.setHeader('= ' + self._patcher_name)
         log(u'==='+_(u'Quests Patched') + u': %d' % (len(patched),))
 
 class CBash_SEWorldEnforcer(_ASEWorldEnforcer,CBash_Patcher):
@@ -860,7 +858,7 @@ class CBash_SEWorldEnforcer(_ASEWorldEnforcer,CBash_Patcher):
         """Will write to log."""
         if not self.isActive: return
         #--Log
-        log.setHeader(u'= ' +self.__class__.name)
+        log.setHeader(u'= ' + self._patcher_name)
         log(u'\n=== '+_(u'Quests Patched'))
         for mod, eids in self.mod_eids.iteritems():
             log(u'* %s: %d' % (mod.s, len(eids)))

Mopy/bash/patcher/__init__.py
@@ -24,8 +24,7 @@
 from collections import namedtuple
 from .. import balt, bolt, bass
 
-PatcherInfo = namedtuple('PatcherInfo',
-                         ['clazz', 'twin_patcher', 'patcher_text'])
+PatcherInfo = namedtuple('PatcherInfo', ['clazz', 'twin_patcher', 'cls_vars'])
 
 def configIsCBash(patchConfigs): ##: belongs to basher but used also in bosh
     for key in patchConfigs:

Mopy/bash/patcher/base.py
@@ -43,26 +43,21 @@ class _Abstract_Patcher(object):
     scanOrder = 10
     editOrder = 10
     group = u'UNDEFINED'
-    name = u'UNDEFINED'
     text = u"UNDEFINED."
-    tip = None
     iiMode = False
 
     def getName(self):
-        """Returns patcher name."""
-        return self.__class__.name
+        """Return patcher name passed in by the gui, needed for logs."""
+        return self._patcher_name
 
     #--Config Phase -----------------------------------------------------------
-    def __init__(self):
+    def __init__(self, patcher_name, patchFile):
         """Initialization of common values to defaults."""
-        self.patchFile = None
-        self.scanOrder = self.__class__.scanOrder
-        self.editOrder = self.__class__.editOrder
         self.isActive = True
+        self.patchFile = patchFile
+        self._patcher_name = patcher_name
         #--Gui stuff
         self.isEnabled = False #--Patcher is enabled.
-        self.gConfigPanel = None
-        # super(Abstract_Patcher, self).__init__()#UNNEEDED (ALWAYS BEFORE obj)
 
     #--Patch Phase ------------------------------------------------------------
     def initPatchFile(self, patchFile):
@@ -100,16 +95,9 @@ class CBash_Patcher(_Abstract_Patcher):
     """Abstract base class for patcher elements performing a CBash patch - must
     be just before Abstract_Patcher in MRO.""" ##: "performing" ? how ?
     # would it make any sense to make getTypes into classmethod ?
-    unloadedText = u""
-    allowUnloaded = True
+    allowUnloaded = True # if True patcher needs a srcs attribute
     scanRequiresChecked = False # if True patcher needs a srcs attribute
     applyRequiresChecked = False # if True patcher needs a srcs attribute
-
-    #--Config Phase -----------------------------------------------------------
-    def __init__(self):
-        super(CBash_Patcher, self).__init__()
-        if not self.allowUnloaded:
-            self.text += self.unloadedText
 
     #--Patch Phase ------------------------------------------------------------
     def getTypes(self):
@@ -135,8 +123,6 @@ class AListPatcher(_Abstract_Patcher):
     """Subclass for patchers that have GUI lists of objects.
 
     :type _patches_set: set[bolt.Path]"""
-    #--Get/Save Config
-    autoKey = None
     # log header to be used if the ListPatcher has mods/files source files
     srcsHeader = u'=== '+ _(u'Source Mods')
     _patches_set = None
@@ -184,9 +170,6 @@ class AAliasesPatcher(_Abstract_Patcher):
     scanOrder = 10
     editOrder = 10
     group = _(u'General')
-    name = _(u"Alias Mod Names")
-    text = _(u"Specify mod aliases for reading CSV source files.")
-    tip = None
 
     #--Patch Phase ------------------------------------------------------------
     def initPatchFile(self, patchFile):
@@ -294,9 +277,6 @@ class APatchMerger(AListPatcher):
     scanOrder = 10
     editOrder = 10
     group = _(u'General')
-    name = _(u'Merge Patches')
-    text = _(u"Merge patch mods into Bashed Patch.")
-    autoKey = {u'Merge'}
 
     #--Patch Phase ------------------------------------------------------------
     def initPatchFile(self, patchFile):
@@ -314,6 +294,3 @@ class AUpdateReferences(AListPatcher):
     scanOrder = 15
     editOrder = 15
     group = _(u'General')
-    name = _(u'Replace Form IDs')
-    text = _(u"Imports Form Id replacers from csv files into the Bashed Patch.")
-    autoKey = {u'Formids'}

commit c3ad6222aad36008e48f93ef00e1fcd769a499f9

FFF patcher_instance -> patcher_type
Fixes List Patch Config, seems to have broken way back in
43ab0c1d6ac2fa3f81fac2cb9e7dfeeab0c6606f?

Mopy/bash/basher/mod_links.py
@@ -1133,7 +1133,7 @@ class Mod_ListPatchConfig(_Mod_BP_Link):
         _gui_patchers = [copy.deepcopy(x) for x in
                     (CBash_gui_patchers if doCBash else PBash_gui_patchers)]
         _gui_patchers.sort(key=lambda a: a.__class__.patcher_name)
-        _gui_patchers.sort(key=lambda a: groupOrder[a.patcher_instance.group])
+        _gui_patchers.sort(key=lambda a: groupOrder[a.patcher_type.group])
         #--Log & Clipboard text
         log = bolt.LogFile(StringIO.StringIO())
         log.setHeader(u'= %s %s' % (self._selected_item, _(u'Config')))